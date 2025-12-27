db8fc7f6507e796c24354644465a098f4c367af4
alexbnls <alexanderbonolis@gmail.com>
2025-12-27 17:50:24 +0100

    Initial commit: SpringBoot project structure.

---
+++ src/main/java/it/unisa/matchpoint/MatchPointApplication.java
@@ -0,0 +1,13 @@
+package it.unisa.matchpoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchPointApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MatchPointApplication.class, args);
+    }
+
+}
