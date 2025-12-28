304ca7d9194cd75b002f131779cc4a271e27eec2
alexbnls <alexanderbonolis@gmail.com>
2025-12-28 18:11:03 +0100

    Added "UtenteRepository" interface (DAO). Closes #8.

---
+++ src/main/java/it/unisa/matchpoint/repository/UtenteRepository.java
@@ -0,0 +1,11 @@
+package it.unisa.matchpoint.repository;
+
+import it.unisa.matchpoint.model.*;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UtenteRepository extends JpaRepository<UtenteRegistrato, String> {
+    // Serve per controllare duplicati mail nel UC_1
+    boolean existsByEmail(String email);
+}
