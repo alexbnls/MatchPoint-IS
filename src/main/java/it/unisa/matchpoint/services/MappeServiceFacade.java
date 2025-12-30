45d7d25b944da22bbc3ce8ac08640bae221d2e98
Marco Galdi <m.galdi26@studenti.unisa.it>
2025-12-30 17:39:54 +0100

    Aggiunto MappeServiceFacade e implementato facade nella creazione evento.

---
+++ src/main/java/it/unisa/matchpoint/services/MappeServiceFacade.java
@@ -0,0 +1,8 @@
+package it.unisa.matchpoint.services;
+
+public interface MappeServiceFacade {
+
+    // Dato un indirizzo, restituisce un array [latitudine, longitudine]
+    // O lancia eccezione se l'indirizzo non esiste
+    Double[] getCoordinate(String indirizzo);
+}
