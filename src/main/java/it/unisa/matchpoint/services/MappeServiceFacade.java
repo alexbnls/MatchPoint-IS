2390cf8cb8e0e37d069f992227a33988703786de
GaetanoAprile <g.aprile5@studenti.unisa.it>
2026-01-02 17:02:45 +0100

    Ho implementato il servizio di geolocalizzazione per quando si crea un EventoSportivo

--- src/main/java/it/unisa/matchpoint/services/MappeServiceFacade.java
+++ src/main/java/it/unisa/matchpoint/services/MappeServiceFacade.java
@@ -5,4 +5,6 @@ public interface MappeServiceFacade {
     // Dato un indirizzo, restituisce un array [latitudine, longitudine]
     // O lancia eccezione se l'indirizzo non esiste
     Double[] getCoordinate(String indirizzo);
+
+    String getIndirizzoDaCoordinate(Double lat, Double lon);
 }
